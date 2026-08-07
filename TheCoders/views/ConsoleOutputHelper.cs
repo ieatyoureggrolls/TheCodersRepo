42aa8e452dbc8b86b734701bc3a70c018d3754bd
Lucas <gamerboy19537@gmail.com>
2026-08-07 08:22:11 -0600

    Stared the Console Output Helper

---
+++ TheCoders/views/ConsoleOutputHelper.cs
@@ -0,0 +1,20 @@
+namespace TheCoders.views;
+
+public static class ConsoleOutputHelper
+{
+    //Print Health bar
+    
+    //Battle Summaru
+    
+    //PrintCrit
+    
+    //Clear screen
+    
+    //PrintWeapons
+    
+    //Print combatant party
+    
+    //Print wave information
+    
+    
+}
