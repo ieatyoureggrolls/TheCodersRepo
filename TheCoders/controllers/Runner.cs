1da0c95f94502cbdf5df28e195700338da822b1f
stunbot11 <143148371+stunbot11@users.noreply.github.com>
2026-08-10 16:35:34 -0600

    Started Battle Loop
    Made a Very basic person class with very basic battle loop. will continue to further these

--- TheCoders/controllers/Runner.cs
+++ TheCoders/controllers/Runner.cs
@@ -1,10 +1,80 @@
+using TheCoders.models;
+
 namespace TheCoders.controllers;
 
 public class Runner
 {
+    private Random random = new Random();
+    public Person[] partyMembers = {new Person(30, 2, 4, true), new Person(25, 3, 6, true), new Person(50, 1, 1, true) };
     public void Run()
     {
         // Entry point of the application
-        Console.WriteLine("Hello, World!");
+        bool isStory = ChooseMode();
+        if (isStory)
+            Story();
+        else
+            Endless();
+    }
+
+    public void Story()
+    {
+        Console.WriteLine("Story");
+    }
+
+    public void Endless()
+    {
+        //Battle();
+        Console.WriteLine("Endless");
+    }
+
+    /// <summary>
+    /// Prompts the user if they want to play story or endless mode
+    /// </summary>
+    /// <returns>True for story | False for endless</returns>
+    private bool ChooseMode()
+    {
+        Console.WriteLine("Story mode: S\nEndless mode: E");
+        string input = Console.ReadLine().ToUpper();
+        return input == "S";
+    }
+
+    /// <summary>
+    /// Handles the main battle programing between the heros and the vilians
+    /// </summary>
+    private void Battle()
+    {
+        List<Person> vilians = AddExperimentalPeople();
+        List<Person> people = new List<Person>();
+        people.AddRange(vilians);
+        people.AddRange(partyMembers);
+        List<Person> attackOrder = people.OrderBy(p => p.speed).ToList();
+
+        foreach (Person person in attackOrder)
+        {
+            int attackChoice;
+            if (person.isHero)
+            {
+                attackChoice = random.Next(vilians.Count);
+                vilians[attackChoice].health -= person.damage;
+            }
+            else if (!person.isHero)
+            {
+                attackChoice = random.Next(vilians.Count);
+                partyMembers[attackChoice].health -= person.damage;
+            }
+        }
+    }
+
+
+
+    private List<Person> AddExperimentalPeople()
+    {
+        List<Person> people = new List<Person>();
+        people.Add(new Person(20, 1, 1, false));
+        people.Add(new Person(5, 3, 4, false));
+        people.Add(new Person(10, 1, 7, false));
+        people.Add(new Person(15, 2, 5, false));
+
+        return people;
     }
 }
---
+++ TheCoders/models/Person.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TheCoders.models
+{
+    public class Person
+    {
+        public int health;
+        public int speed;
+        public int damage;
+        public bool isHero;
+
+        public Person(int health, int speed, int damage, bool isHero)
+        {
+            this.health = health;
+            this.speed = speed;
+            this.damage = damage;
+            this.isHero = isHero;
+        }
+    }
+}
