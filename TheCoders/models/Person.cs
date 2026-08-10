fc1b013411d649aaf57cba45f5f6742112641957
ieatyoureggrolls <gamerboy19537@gmail.com>
2026-08-10 16:52:14 -0600

    Added maxHealth and currentHealth to the person class. This is needed for the PrintHealthBar method

--- TheCoders/models/Person.cs
+++ TheCoders/models/Person.cs
@@ -6,14 +6,17 @@ namespace TheCoders.models
 {
     public class Person
     {
-        public int health;
+        public int currentHealth;
+        public int maxHealth;
         public int speed;
         public int damage;
         public bool isHero;
+        
 
-        public Person(int health, int speed, int damage, bool isHero)
+        public Person(int maxHealth, int speed, int damage, bool isHero)
         {
-            this.health = health;
+            this.maxHealth = maxHealth;
+            currentHealth = maxHealth;
             this.speed = speed;
             this.damage = damage;
             this.isHero = isHero;
