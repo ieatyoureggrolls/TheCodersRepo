604b670327fb81e544ebf91c082156a83392041a
ieatyoureggrolls <gamerboy19537@gmail.com>
2026-08-11 16:44:53 -0600

    Finished the PrinHealthBar method

--- TheCoders/views/ConsoleOutputHelper.cs
+++ TheCoders/views/ConsoleOutputHelper.cs
@@ -6,29 +6,44 @@ namespace TheCoders.views;
 
 public static class ConsoleOutputHelper
 {
-    //Print Health bar
     /// <summary>
     /// Prints a health bar made of squares corresponding to how much health a player has
     /// </summary>
     /// <param name="person"> The Person object whose health will be influencing the health bar</param>
     public static void PrintHealthBar(Person person)
     {
-      
-        int maxHealth = person.MaxHealth;
-        int currentHealth = person.CurrentHealth;
-        int numOfHealthSquares = 10;
-        int numOfFilledSquares = currentHealth /  numOfHealthSquares;
-        Console.WriteLine($"Health: {currentHealth} / {maxHealth}");
-        Console.WriteLine($"Number of health squares: {numOfHealthSquares}");
-        Console.WriteLine($"Number of filled squares: {numOfFilledSquares}");
-        
+        //Value errors are handled in Person setters
 
+        string square = "\u25A0";
+        double maxHealth = person.MaxHealth;
+        double currentHealth = person.CurrentHealth;
+
+        int numOfHealthSquares = 10;
+
+        //Gets health percentage, then uses it to calculate how many filled and empty squares
+        double healthPercentage = currentHealth / maxHealth * 100;
+        int numOfFilledSquares = (int)Math.Round(numOfHealthSquares * (healthPercentage / 100));
+        int numOfEmptySquares = numOfHealthSquares - numOfFilledSquares;
+
+        for (int i = 0; i < numOfFilledSquares; i++)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write(square);
+            Console.ResetColor();
+        }
+
+        for (int i = 0; i < numOfEmptySquares; i++)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write(square);
+            Console.ResetColor();
+        }
     }
-    
+
     //Battle Summary
-    
+
     //PrintCrit
-    
+
     /// <summary>
     /// Clears Terminal of all text
     /// </summary>
@@ -36,12 +51,10 @@ public static class ConsoleOutputHelper
     {
         Console.Clear();
     }
-    
+
     //PrintWeapons
-    
+
     //Print combatant party
-    
+
     //Print wave information
-    
-    
 }
