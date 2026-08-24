2f536e27bd0d905e3ac8e0017e23121f24e3b8f2
FinalAquaticZenith <auswoods2006@gmail.com>
2026-08-24 16:09:35 -0600

    added overloaded createWeapon() that takes in an int

--- TheCoders/models/Weapon.cs
+++ TheCoders/models/Weapon.cs
@@ -34,6 +34,7 @@ namespace TheCoders
         private int value = 0;
         private int level = 1;
         private int repairCost = 0;
+        private int upgradeCost = 0;
 
         public String getName() { return name; }
         public int getRepair() {  return repairCost; }
@@ -48,6 +49,7 @@ namespace TheCoders
         public int getEnchantPoints() { return enchantPoints; }
         public bool getBroken() { return isBroken; }
         public bool isHalf() {  return halfDurablility; }
+        public int getUpgradeCost() { return upgradeCost; }
 
        
         public Weapon(Blade blade, Handle handle)
@@ -57,6 +59,7 @@ namespace TheCoders
             durability = blade.getDurability() + handle.getDurability();
             maxDurability = blade.getDurability()+handle.getDurability();
             value = blade.getPrice() + handle.getPrice();
+            upgradeCost = (blade.getPrice()+handle.getPrice())/2;
 
             if (blade.getThreshold() > handle.getThreshold())
             {
@@ -77,8 +80,228 @@ namespace TheCoders
                 rarity = handle.getRarity();
             }
         }
-
-
+        //asks for shape of the blade and length of the handle, as well as materials for both. Then returns a weapon with those parameters. The amount of materials shown is dependant on materialCount
+        public static Weapon createWeapon(int materialCount)
+        {
+            String BMaterialStr = "Select a material for the blade:";
+            String HMaterialStr = "Select a material for the handle:";
+            Weapon sword;
+            Blade blade;
+            Handle handle;
+            Blade.BladeType bladetype;
+            String bladetypeStr = "";
+            Handle.HandleType handletype;
+            String handletypeStr = "";
+            Pieces.Material handleMaterial;
+            String handleMaterialStr = "";
+            Pieces.Material bladeMaterial;
+            String bladeMaterialStr = "";
+            bool confirm;
+            switch (materialCount)
+            {
+                case 1:
+                    BMaterialStr += "\n1. Wood\n";
+                    HMaterialStr += "\n1. Wood\n";
+                    break; 
+                case 2:
+                    BMaterialStr += "\n1. Wood\n2. Stone\n";
+                    HMaterialStr += "\n1. Wood\n2. Stone\n";
+                    break;
+                case 3:
+                    BMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n";
+                    HMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n";
+                    break; 
+                case 4:
+                    BMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n4. Steel\n";
+                    HMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n4. Steel\n";
+                    break; 
+                case 5:
+                    BMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n4. Steel\n5. Gold\n";
+                    HMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n4. Steel\n5. Gold\n";
+                    break; 
+                case 6:
+                    BMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n4. Steel\n5. Gold\n6. Adamantine\n";
+                    HMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n4. Steel\n5. Gold\n6. Adamantine\n";
+                    break; 
+                case 7:
+                    BMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n4. Steel\n5. Gold\n6. Adamantine\n7. Mithril\n";
+                    HMaterialStr += "\n1. Wood\n2. Stone\n3. Bronze\n4. Steel\n5. Gold\n6. Adamantine\n7. Mithril\n";
+                    break; 
+                default: 
+                    break;
+            }
+            do
+            {
+                int bladetypeint = CIO.PromptForInt("Select a shape for the blade:\n1. Short\n2. Long\n3. Great\n", 1, 3);
+                int bladeMaterialInt = CIO.PromptForInt(BMaterialStr, 1, materialCount);
+                int handleTypeInt = CIO.PromptForInt("Select a length for the handle:\n1. Short\n2. Medium\n3. Long\n", 1, 3);
+                int handleMaterialInt = CIO.PromptForInt(HMaterialStr, 1, materialCount);
+                switch (bladetypeint)
+                {
+                    case 1:
+                        bladetypeStr = "short";
+                        break;
+                    case 2:
+                        bladetypeStr = "long";
+                        break;
+                    case 3:
+                        bladetypeStr = "great";
+                        break;
+                }
+                switch (bladeMaterialInt)
+                {
+                    case 1:
+                        bladeMaterialStr = "wooden";
+                        break;
+                    case 2:
+                        bladeMaterialStr = "stone";
+                        break;
+                    case 3:
+                        bladeMaterialStr = "bronze";
+                        break;
+                    case 4:
+                        bladeMaterialStr = "steel";
+                        break;
+                    case 5:
+                        bladeMaterialStr = "gold";
+                        break;
+                    case 6:
+                        bladeMaterialStr = "adamantine";
+                        break;
+                    case 7:
+                        bladeMaterialStr = "mithril";
+                        break;
+                }
+                switch (handleTypeInt)
+                {
+                    case 1:
+                        handletypeStr = "short";
+                        break;
+                    case 2:
+                        handletypeStr = "medium";
+                        break;
+                    case 3:
+                        handletypeStr = "long";
+                        break;
+                }
+                switch (handleMaterialInt)
+                {
+                    case 1:
+                        handleMaterialStr = "wooden";
+                        break;
+                    case 2:
+                        handleMaterialStr = "stone";
+                        break;
+                    case 3:
+                        handleMaterialStr = "bronze";
+                        break;
+                    case 4:
+                        handleMaterialStr = "steel";
+                        break;
+                    case 5:
+                        handleMaterialStr = "gold";
+                        break;
+                    case 6:
+                        handleMaterialStr = "adamantine";
+                        break;
+                    case 7:
+                        handleMaterialStr = "mithril";
+                        break;
+                }
+                switch (bladetypeStr.Trim().ToLower())
+                {
+                    case "short":
+                        bladetype = Blade.BladeType.Short;
+                        break;
+                    case "long":
+                        bladetype = Blade.BladeType.Long;
+                        break;
+                    case "great":
+                        bladetype = Blade.BladeType.Great;
+                        break;
+                    default:
+                        bladetype = Blade.BladeType.Short;
+                        break;
+                }
+                switch (bladeMaterialStr.Trim().ToLower())
+                {
+                    case "wooden":
+                        bladeMaterial = Pieces.Material.wood;
+                        break;
+                    case "stone":
+                        bladeMaterial = Pieces.Material.stone;
+                        break;
+                    case "bronze":
+                        bladeMaterial = Pieces.Material.bronze;
+                        break;
+                    case "steel":
+                        bladeMaterial = Pieces.Material.steel;
+                        break;
+                    case "gold":
+                        bladeMaterial = Pieces.Material.gold;
+                        break;
+                    case "adamantine":
+                        bladeMaterial = Pieces.Material.adamantine;
+                        break;
+                    case "mithril":
+                        bladeMaterial = Pieces.Material.mithril;
+                        break;
+                    default:
+                        bladeMaterial = Pieces.Material.wood;
+                        break;
+                }
+                switch (handleMaterialStr.Trim().ToLower())
+                {
+                    case "wooden":
+                        handleMaterial = Pieces.Material.wood;
+                        break;
+                    case "stone":
+                        handleMaterial = Pieces.Material.stone;
+                        break;
+                    case "bronze":
+                        handleMaterial = Pieces.Material.bronze;
+                        break;
+                    case "steel":
+                        handleMaterial = Pieces.Material.steel;
+                        break;
+                    case "gold":
+                        handleMaterial = Pieces.Material.gold;
+                        break;
+                    case "adamantine":
+                        handleMaterial = Pieces.Material.adamantine;
+                        break;
+                    case "mithril":
+                        handleMaterial = Pieces.Material.mithril;
+                        break;
+                    default:
+                        handleMaterial = Pieces.Material.wood;
+                        break;
+                }
+                switch (handletypeStr.Trim().ToLower())
+                {
+                    case "short":
+                        handletype = Handle.HandleType.Short;
+                        break;
+                    case "long":
+                        handletype = Handle.HandleType.Long;
+                        break;
+                    case "medium":
+                        handletype = Handle.HandleType.Medium;
+                        break;
+                    default:
+                        handletype = Handle.HandleType.Short;
+                        break;
+                }
+                blade = new Blade(bladetype, bladeMaterial);
+                handle = new Handle(handletype, handleMaterial);
+                sword = new Weapon(blade, handle);
+                Console.WriteLine($"You chose the {bladeMaterialStr} {bladetypeStr}sword blade and the {handleMaterialStr} {handletypeStr} handle. Is this what you want?");
+                sword.displayWeaponInfo();
+                confirm = CIO.PromptForBool("1.Yes\n2.No", "1", "2");
+            } while (!confirm);
+            sword.name = CIO.PromptForInput("What will you name this weapon?", false);
+            return sword;
+        }
 
         //asks for shape of the blade and length of the handle, as well as materials for both. Then returns a weapon with those parameters.
         public static Weapon createWeapon()
@@ -285,7 +508,7 @@ namespace TheCoders
         //it will ask if you want to replace the enchantment with the new one.
         public void addEnchantment()
         {
-            
+            bool elemental = false;
             bool conf = false;
             bool replace = false;
             Enchantments type = new Enchantments(Enchantments.EnchantmentType.none);
@@ -299,12 +522,15 @@ namespace TheCoders
                         return;
                     case 1:
                         type.setEnchantmentType(Enchantments.EnchantmentType.fire);
+                        elemental = true;
                         break;
                     case 2:
                         type.setEnchantmentType(Enchantments.EnchantmentType.water);
+                        elemental = true;
                         break;
                     case 3:
                         type.setEnchantmentType(Enchantments.EnchantmentType.earth);
+                        elemental = true;
                         break;
                     case 4:
                         type.setEnchantmentType(Enchantments.EnchantmentType.lifesteal);
@@ -338,7 +564,7 @@ namespace TheCoders
                         conf = false;
                         type.setEnchantmentType(Enchantments.EnchantmentType.none);
                     }
-                    if (damageIndex != null && type.getEnchantmentType() == Enchantments.EnchantmentType.fire || damageIndex != null && type.getEnchantmentType() == Enchantments.EnchantmentType.water || damageIndex != null && type.getEnchantmentType() == Enchantments.EnchantmentType.earth)
+                    if (damageIndex != null && elemental)
                     {
                         conf = CIO.PromptForBool("You already have an elemental enchantment. Adding the selected enchantment will remove the previous elemental enchantment. Are you sure you want to do this?\n1. Yes\n2. No", "1", "2");
                         if (conf)
@@ -354,7 +580,7 @@ namespace TheCoders
             } while (!conf);
             if (replace)
             {
-                setEnchantment(type, damageIndex.Value);
+                setEnchantment(type, damageIndex!.Value);
             }
             else
             {
@@ -484,6 +710,7 @@ namespace TheCoders
             value += 5;
             attack += 5;
             speed += 5;
+            upgradeCost += 5;
         }
 
         //sets the weapon name to newName
