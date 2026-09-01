1ae3a97433d6e525ea25bbcfca4a79fa02c755fe
Misticburrito <137470892+Misticburrito@users.noreply.github.com>
2026-08-31 21:01:52 -0600

    changes
    testing and bug fixing

--- TheCoders/models/Story.cs
+++ TheCoders/models/Story.cs
@@ -22,9 +22,18 @@ namespace TheCoders.models
 
         }
 
-        
-        
-        
+        /*
+         * bugs and missing feature
+         * player choice cannot exit once an option is selected
+         * when its asking which hero to pick it looks ugly and shows too much uneccessary information
+         * for some reason it stopped me at level 5 and didn't let me go to level 6
+         * the gold that the user has is not displayed
+         * weapons have no cost so its meta to make weapons then heal heroes
+         * 
+         * 
+         * */
+
+
 
 
         private static void TakeToLevel(int levelSelect)
@@ -88,9 +97,11 @@ namespace TheCoders.models
                 case (5):
                     Level5();
                     break;
+
                 case (6):
                     Level6();
                     break;
+
                 case (7):
                     Level7();
                     break;
@@ -114,7 +125,7 @@ namespace TheCoders.models
 
             const int currentLevel = 1;
 
-           
+
             string intro = $"You are the greatest blacksmith in this village help the heros by making weapons for them the team consists of {theParty.Length} heroes. You must make a weapon for all of them. Take a look, the enemies are quickly approaching!";
             COH.PrintStory(intro, 25);
             COH.PrintCombatantParty(enemies);
@@ -128,7 +139,7 @@ namespace TheCoders.models
 
             COH.PrintStory("I don't have time to explain things to you right now take this sword!", 7);
             tutorialWeapon.displayWeaponInfo();
-        
+
             COH.PrintStory("Now pick which hero will get this weapon, once you do the battle will begin", 5);
 
             WhoGetsAWeapon(theParty, tutorialWeapon);
@@ -148,10 +159,10 @@ namespace TheCoders.models
 
         private static void Level2()
         {
-      
+
 
             COH.ClearScreen();
-            Enemy[] enemies = EnemyGenerator.GenerateEnemies(1,2);
+            Enemy[] enemies = EnemyGenerator.GenerateEnemies(1, 2);
             const int currentLevel = 2;
 
             COH.PrintStory("More enemies are coming, one hero won't be enought to hold them off its time for you to make a weapon too, these guys are tought try making something better than a wooden sword ", 20);
@@ -168,7 +179,7 @@ namespace TheCoders.models
             COH.PrintStory("thats a great weapon for our new hero,\n 2 vs 2 is much more of a fair fight", 7);
             WhoGetsAWeapon(theParty, newWeapon);
 
-            
+
 
             LevelLoop.Battle(enemies.ToList(), theParty);
 
@@ -190,7 +201,7 @@ namespace TheCoders.models
         private static void Level3()
         {
 
-            
+
 
             COH.ClearScreen();
             Enemy[] enemies = EnemyGenerator.GenerateEnemies(1, 2);
@@ -198,11 +209,11 @@ namespace TheCoders.models
             COH.PrintCombatantParty(enemies);
 
             theParty[0].heldWeapon.displayWeaponInfo();
-         
-            
+
+
             COH.PrintStory($"you can't win with a broken weapon try repairing {theParty[0].Name}'s weapon", 7);
             RepairWeapon();
-            COH.PrintStory("good now the heroes are able to continue fighting",7);
+            COH.PrintStory("good now the heroes are able to continue fighting", 7);
 
             LevelLoop.Battle(enemies.ToList(), theParty);
 
@@ -222,7 +233,7 @@ namespace TheCoders.models
         private static void Level4()
         {
 
-            
+
 
             COH.ClearScreen();
             Enemy[] enemies = EnemyGenerator.GenerateEnemies(2, 2);
@@ -257,18 +268,18 @@ namespace TheCoders.models
         private static void Level5()
         {
 
-            
-            
+
+
 
             COH.ClearScreen();
             const int currentLevel = 5;
             Enemy[] enemies = EnemyGenerator.GenerateEnemies(1, 5);
-            
+
             COH.PrintStory("Good job defeating that boss, now that the village is defended its time to go on the offensive", 7);
 
             COH.PrintCombatantParty(enemies);
 
-            COH.PrintStory("\n\nwow thats a lot of them good luck you know what to do",7);
+            COH.PrintStory("\n\nwow thats a lot of them good luck you know what to do", 7);
 
             playerChoice();
 
@@ -300,7 +311,7 @@ namespace TheCoders.models
             COH.PrintStory("I think this is the last of the evil king's defences, after this he'll be left defenceless ", 7);
 
             COH.PrintCombatantParty(enemies);
-                        
+
             playerChoice();
 
             LevelLoop.Battle(enemies.ToList(), theParty);
@@ -324,13 +335,13 @@ namespace TheCoders.models
 
 
             COH.ClearScreen();
-            
+
             Enemy King = EnemyGenerator.GenerateOneEnemy(3, true);
             Enemy Queen = EnemyGenerator.GenerateOneEnemy(3, true);
             King.Name = "King George";
             Queen.Name = "Queen Elisabeth";
-            Enemy[] army = new Enemy[2] { King,Queen};
-           
+            Enemy[] army = new Enemy[2] { King, Queen };
+
             const int currentLevel = 7;
             COH.PrintCombatantParty(army);
 
@@ -426,21 +437,22 @@ namespace TheCoders.models
 
         }
 
-        private static void HealPlayer() 
+        private static void HealPlayer()
         {
             int healCost = 750;
-            if (LevelLoop.gold < healCost) {
+            if (LevelLoop.gold > healCost)
+            {
                 int theslot = ChooseHero();
                 theParty[theslot].CurrentHealth = theParty[theslot].MaxHealth;
                 LevelLoop.gold -= healCost;
             }
-            else 
+            else
             {
-                Console.WriteLine("You do not have enought gold to heal this ally");
+                Console.WriteLine($"You do not have enought gold to heal any ally you need 750 and have {LevelLoop.gold}");
             }
-            
 
-            
+
+
 
         }
 
@@ -456,18 +468,18 @@ namespace TheCoders.models
 
         }
 
-        
 
 
-        public static void playerChoice() 
+
+        public static void playerChoice()
         {
 
-                           
+
             bool isCrafting = true;
             do
             {
                 Console.WriteLine("What would you like to do?");
-                string[] possibleMenus = { "Skip", "Craft Weapon", "Repair Weapon", "Replace Weapon", "HealHero"};
+                string[] possibleMenus = { "Skip", "Craft Weapon", "Repair Weapon", "Replace Weapon", "HealHero" };
                 int selection = CIO.PromptForMenuSelection(possibleMenus, false);
                 switch (selection)
                 {
@@ -476,39 +488,49 @@ namespace TheCoders.models
                         break;
                     case 2:
                         MakeWeapon();
-                        
+
                         break;
                     case 3:
-                        RepairWeapon(); 
+                        RepairWeapon();
                         break;
                     case 4:
-                        ReplaceWeapon(); 
+                        ReplaceWeapon();
                         break;
                     case 5:
                         HealPlayer();
                         break;
-                   
-                 
+                    case 6:
+                        UpgradeWeapon();
+                        break;
+                    case 7:
+                        CheckGold();
+                        break;
+
                 }
             } while (isCrafting);
         }
 
-        private static void ReplaceWeapon() 
+        private static void CheckGold()
+        {
+            COH.PrintStory($"{LevelLoop.gold}", 4);
+        }
+
+        private static void ReplaceWeapon()
         {
 
-            COH.PrintStory("Which weapon would you like to equip",7);
+            COH.PrintStory("Which weapon would you like to equip", 7);
             List<string> weaponNames = new List<string>();
-            foreach (Weapon weapon in weaponStorage) 
+            foreach (Weapon weapon in weaponStorage)
             {
                 weaponNames.Add(weapon.getName());
             }
 
-           int selection = (CIO.PromptForMenuSelection(weaponNames,true)-1);
-           Weapon chosenWeapon = weaponStorage.ElementAt(selection);
-           int heroSlot = ChooseHero();
+            int selection = (CIO.PromptForMenuSelection(weaponNames, true) - 1);
+            Weapon chosenWeapon = weaponStorage.ElementAt(selection);
+            int heroSlot = ChooseHero();
 
 
-            if (heroSlot >= 0 && heroSlot < (theParty.Length-1))
+            if (heroSlot >= 0 && heroSlot < (theParty.Length - 1))
             {
                 weaponStorage.Add(theParty[heroSlot].heldWeapon);
                 theParty[heroSlot].EquipWeapon(chosenWeapon);
@@ -519,31 +541,51 @@ namespace TheCoders.models
                 Console.WriteLine("Invalid hero selection.");
             }
 
-            
+
 
 
 
             Console.WriteLine("\n\n");
         }
 
-        private static void RepairWeapon()
+        
+
+        public static void RepairWeapon()
         {
-            int heroSlot = ChooseHero();
 
-            if (theParty[heroSlot].heldWeapon != null)
+            Weapon weaponToRepair = CIO.PromptForWeaponFromPerson("Whoes weapon would you like to Repair?", theParty);
+
+            bool isNull = true;
+
+            do
             {
+                if (weaponToRepair == null)
+                {
 
-                theParty[heroSlot].heldWeapon.repairWeapon();
-            }
-            else
+                    weaponToRepair = CIO.PromptForWeaponFromPerson("that hero doesn't have a weapon pick another", theParty);
+                }
+                else { isNull = false; }
+            } while (isNull);
+
+
+            bool doBuy = CIO.PromptForBool($"it will cost {weaponToRepair.getRepair()} gold\nAre you sure?\n", "Yes", "No");
+            if (weaponToRepair != null && doBuy)
             {
-                Console.WriteLine($"the hero {theParty[heroSlot].Name} does not have a weapon that needs repairing");
+                if (weaponToRepair.getRepair() > LevelLoop.gold)
+                {
+                    Console.WriteLine($"You don't have enough gold to upgrade this weapon you need {weaponToRepair.getRepair()}");
+                }
+                else
+                {
+                    weaponToRepair.repairWeapon();
+                    LevelLoop.gold -= weaponToRepair.getRepair();
+                }
             }
 
-            Console.WriteLine("\n\n");
         }
 
-        private static void MakeWeapon() 
+
+        private static void MakeWeapon()
         {
             int limit = (LevelLoop.gold / 1000) + 2;
 
@@ -553,20 +595,44 @@ namespace TheCoders.models
             if (theParty[heroSlot].heldWeapon != null)
             {
                 weaponStorage.Add(theParty[heroSlot].heldWeapon);
-             
+
             }
 
             theParty[heroSlot].EquipWeapon(newWeapon);
             Console.WriteLine("\n\n");
-            
+
 
         }
-        private static int ChooseHero() 
+
+
+        public static void UpgradeWeapon()
+        {
+
+            Weapon weaponToUpgrade = CIO.PromptForWeaponFromPerson("Whoes weapon would you like to upgrade?", theParty);
+            bool doBuy = CIO.PromptForBool($"it will cost {weaponToUpgrade.getUpgradeCost()} gold\nAre you sure?\n", "Yes", "No");
+            if (weaponToUpgrade != null && doBuy)
+            {
+                if (weaponToUpgrade.getUpgradeCost() > LevelLoop.gold)
+                {
+                    Console.WriteLine($"You don't have enough gold to upgrade this weapon you need {weaponToUpgrade.getUpgradeCost()}");
+                }
+                else
+                {
+                    weaponToUpgrade.upgradeWeapon();
+                    LevelLoop.gold -= weaponToUpgrade.getUpgradeCost();
+                }
+            }
+
+        }
+
+
+
+        private static int ChooseHero()
         {
 
             COH.PrintHeroNames(theParty);
-            int heroSlot = CIO.PromptForInt($"which heroes from slot 1 to {theParty.Length} do you want to choose", 1, theParty.Length);
-            return heroSlot-1;
+            int heroSlot = CIO.PromptForInt($"which hero from slot 1 to {theParty.Length} do you want to choose", 1, theParty.Length);
+            return heroSlot - 1;
 
         }
 
@@ -587,7 +653,7 @@ namespace TheCoders.models
             theRunner.Run();
         }
 
-      
+
 
     }
 }
